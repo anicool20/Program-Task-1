5512c326855e19a7c03c415ae66342082d6c6ffb
anicool20 <45395114+anicool20@users.noreply.github.com>
2018-11-27 10:30:23 -0500

    Added Print method, made some changed

--- listchange.java
+++ listchange.java
@@ -7,12 +7,21 @@ public class listchange{
 			list.add(i);
 			list2.add(i);
 		}
-		combine(list, list2);
+		print(combine(list, list2));
 	}
 	public static ArrayList <Integer> combine(ArrayList <Integer> listA, ArrayList <Integer> listB){
 		for(int i = 0; i < listB.size();i++){
 			listA.add(listB.get(i));
 		}
 		return listA;
+	}
+	public static void print(ArrayList <Integer> listP){
+		for(int i = 0; i < listP.size();i++){
+			if(i == listP.size()-1)
+			System.out.println(listP.get(i));
+			else
+			System.out.print(listP.get(i)+", ");
+		}
+		System.out.println();
 	}
 }
