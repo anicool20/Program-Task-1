bbcdf88c1818406c9a1254c06fa45f331ea54c50
anicool20 <45395114+anicool20@users.noreply.github.com>
2018-11-27 10:15:31 -0500

    main method done

---
+++ listchange.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class listchange{
+	public static void main(String[]args){
+		ArrayList <Integer> list = new ArrayList <>();
+		ArrayList <Integer> list2 = new ArrayList <>();
+		for(int i = 0; i < 10; i++){
+			list.add(i);
+			list2.add(i);
+		}
+	}
+}
